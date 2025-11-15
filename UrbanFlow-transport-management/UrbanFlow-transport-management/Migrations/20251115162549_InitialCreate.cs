ddf0732dfa57b44821ca54dcbc2b963fd7c85740
mathiasv2 <dumas.mathias@outlook.com>
2025-11-15 17:26:05 +0100

    feature: migrations file

---
+++ UrbanFlow-transport-management/UrbanFlow-transport-management/Migrations/20251115162549_InitialCreate.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace UrbanFlow_transport_management.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RouteTypes",
+                columns: table => new
+                {
+                    RouteTypeId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RouteTypes", x => x.RouteTypeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vehicules",
+                columns: table => new
+                {
+                    VehiculeId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    RegistrationNumber = table.Column<string>(type: "text", nullable: false),
+                    RouteTypeId = table.Column<int>(type: "integer", nullable: false),
+                    Brand = table.Column<string>(type: "text", nullable: false),
+                    Model = table.Column<string>(type: "text", nullable: false),
+                    ServiceDebut = table.Column<DateOnly>(type: "date", nullable: false),
+                    SeatsCount = table.Column<int>(type: "integer", nullable: false),
+                    MaxCapacity = table.Column<int>(type: "integer", nullable: false),
+                    Status = table.Column<string>(type: "text", nullable: false),
+                    NextMaintenance = table.Column<DateOnly>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicules", x => x.VehiculeId);
+                    table.ForeignKey(
+                        name: "FK_Vehicules_RouteTypes_RouteTypeId",
+                        column: x => x.RouteTypeId,
+                        principalTable: "RouteTypes",
+                        principalColumn: "RouteTypeId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VehiculePositions",
+                columns: table => new
+                {
+                    VehiculePositionId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    VehiculeId = table.Column<int>(type: "integer", nullable: false),
+                    Longitude = table.Column<decimal>(type: "numeric(9,6)", nullable: false),
+                    Latitude = table.Column<decimal>(type: "numeric(9,6)", nullable: false),
+                    TripId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VehiculePositions", x => x.VehiculePositionId);
+                    table.ForeignKey(
+                        name: "FK_VehiculePositions_Vehicules_VehiculeId",
+                        column: x => x.VehiculeId,
+                        principalTable: "Vehicules",
+                        principalColumn: "VehiculeId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VehiculePositions_VehiculeId",
+                table: "VehiculePositions",
+                column: "VehiculeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vehicules_RouteTypeId",
+                table: "Vehicules",
+                column: "RouteTypeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VehiculePositions");
+
+            migrationBuilder.DropTable(
+                name: "Vehicules");
+
+            migrationBuilder.DropTable(
+                name: "RouteTypes");
+        }
+    }
+}
