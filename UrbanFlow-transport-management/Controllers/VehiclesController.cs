42a838bb3ba0d8a7b4e2b53a6cc553c8629d25e1
mathiasv2 <dumas.mathias@outlook.com>
2026-04-02 16:09:51 +0200

    scoped in program.cs + controller

---
+++ UrbanFlow-transport-management/Controllers/VehiclesController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using UrbanFlow_transport_management.DTO.Vehicule;
+using UrbanFlow_transport_management.Repository;
+
+namespace UrbanFlow_transport_management.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class VehiclesController(VehicleRepository repo) : Controller
+{
+    [HttpGet("filter/{id}")]
+    public async Task<IActionResult> FilterRoutes([FromQuery] VehicleFilterDto filter, int id)
+    {
+        var vehicles = await repo.GetVehiclesByAgencyIdWithFilters(filter, id);
+        if (!vehicles.Any()) return NotFound();
+        return Ok(vehicles);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateVehicle([FromBody] CreateVehiculeDto vehicle)
+    {
+        await repo.CreateVehicleAsync(vehicle);
+        return Ok(new 
+        {
+            message = "Vehicle created"
+        });
+    }
+    
+    
+}
