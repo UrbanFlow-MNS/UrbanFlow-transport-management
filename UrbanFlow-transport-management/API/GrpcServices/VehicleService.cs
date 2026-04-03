54183b18c3cbb1cb2ae960195fd5cda1253aed65
mathiasv2 <dumas.mathias@outlook.com>
2026-04-03 16:23:42 +0200

    feat: routetype grpc config

---
+++ UrbanFlow-transport-management/API/GrpcServices/VehicleService.cs
@@ -0,0 +1,18 @@
+using Grpc.Core;
+using UrbanFlow_transport_management.DTO.RouteType;
+using UrbanFlow_transport_management.Repository;
+using UrbanFlow_trips;
+
+namespace UrbanFlow_transport_management.API.GrpcServices;
+
+public class VehicleService(IRouteTypeRepository repo) : Vehicler.VehiclerBase
+{
+    public override async Task<VehicleResponse> FindById(VehicleRequest request, ServerCallContext context)
+    {
+        var trip = await repo.GetRouteTypeById(request.Id);
+        return new VehicleResponse()
+        {
+            VehicleName = trip.Name,
+        };
+    }
+}
--- UrbanFlow-transport-management/Domain/Interfaces/IRouteTypeRepository.cs
+++ UrbanFlow-transport-management/Domain/Interfaces/IRouteTypeRepository.cs
@@ -6,6 +6,7 @@ public interface IRouteTypeRepository
 {
     Task CreateRouteTypeAsync(CreateRouteTypeDto routeTypeDto);
     Task<List<GetRouteTypeDto>> GetAllRouteTypesByAgencyIdAsync(int id);
-    Task DeleteRouteType(int id);
+    //Task DeleteRouteType(int id);
     Task UpdateRouteTypeAsync(int id, UpdateRouteTypeDto routeTypeDto);
+    Task<GetRouteTypeDto?> GetRouteTypeById(int id);
 }
